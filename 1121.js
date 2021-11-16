509111d067393232a454228af36500982abf2622
Patricia Gonzalez <waltraute@hotmail.com>
2021-11-16 09:55:10 +0100

    Entrega 1.1: Functions & Template Literals

--- 1121.js
+++
@@ -1,6 +0,0 @@
-//Mostra per consola el nom i cognoms de l'usuari mitjançant template literals, guardant-los en variables i referenciant-les en la impressió del missatge.
-var name = `Patricia Gonzalez`;
-
-function user (name) {
-console.log(name);
-}
