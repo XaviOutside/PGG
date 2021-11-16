1a59bade8ef8e89ea4d4fb67f6fbafd0eb20f54e
Patricia Gonzalez <waltraute@hotmail.com>
2021-11-16 09:50:53 +0100

    initial commit

---
+++ 1111.js
@@ -0,0 +1,11 @@
+//Crea una funció que mostri per consola el nom d'usuari al invocar-la passant-li el nom com a paràmetre.
+
+   var name;
+
+   function showName(name) {
+      console.log ("Hi" + name);
+      }
+
+  showName("Patricia");
+
+  
