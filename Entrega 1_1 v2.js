0efa519e500b986511b9d81ae1509f7a172444ab
Patricia <waltraute@hotmail.com>
2021-11-26 21:11:34 +0100

    fet

--- Entrega 1_1 v2.js
+++ Entrega 1_1 v2.js
@@ -38,29 +38,15 @@ Crea una matriu de deu funcions i emplena-la mitjançant un bucle de manera que 
 per la consola. Invoca cada funció de l'array iterativament. Haurà de mostrar-se per consola el compte del 0 
 al 9 deu vegades.*/
 
-// TODO  crea matriu buida
-
 let arrayFunc = new Array(9); //crea array buida q tindrà 10 posicions
 
-// TODO creo funcion q cree funciones
+function contar() {
+    console.log(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+}
 
-for (item)
-
-function createFunc(){
-
-
-    return this.altura * this.anchura;
-
-
-// todo omplir-la amb bucle (exemple s'omple amb "func1")
-
-
-
-arr.fill(func1[ start = 0[ end = 9]])
-
-arrayFunc.forEach(function(item,index,arr){
-    console.log("Posición " + index + ": " + item);
-});
+for (var i = 0; i <= 9; i++) {
+    arrayFunc.push(contar());
+}
 
 /*N3 E2
 Crea una funció anònima autoinvocable igualada a una variable que mostri per consola el nom de l'usuari
@@ -69,5 +55,3 @@ rebut com a paràmetre.*/
 saludar = (function (name) {
     console.log(`¡Hola, ${name}!`);
 })("Patricia");
-
-
